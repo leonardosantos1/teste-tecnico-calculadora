0d446982984892a572f8b2b396c5c414e56a6fa9
Leonardo Santos <leonardo.santos574@hotmail.com>
2023-04-17 18:42:25 -0300

    implement signup and sucess-signup html page, signup user js

--- teste-tecnico-backend-l5network/src/controllers/User/SignUpController.ts
+++ teste-tecnico-backend-l5network/src/controllers/User/SignUpController.ts
@@ -1,17 +1,25 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { SignUpService } from "../../services/User/SignUpService";
+import { ApplicationError } from "../../error/ApplicationError";
 
 class SignUpController {
   async handle(req: Request, res: Response): Promise<Response> {
     
-    const { name, email, password } = req.body;
+      try{
+        const { name, email, password } = req.body;
 
-    const signUpService = container.resolve(SignUpService);
+        const signUpService = container.resolve(SignUpService);
+    
+        await signUpService.execute({ name, email, password });
+    
+        return res.status(201).send();
+      }catch(err){
+        console.log(err);
+        throw new ApplicationError(err, 400);
+      }
 
-    await signUpService.execute({ name, email, password });
-
-    return res.status(201).send();
+    
   }
 }
 
--- teste-tecnico-frontend-l5network/js/main.js
+++ teste-tecnico-frontend-l5network/js/main.js
@@ -1,5 +1,6 @@
 var api_url_login = "http://localhost:3001/login";
 var api_url_math = "http://localhost:3001/maths";
+var api_url_signup = "http://localhost:3001/signup"
 
 var buttonLogin = $("#button-login");
 
@@ -82,3 +83,54 @@ $(".clear-button").click(function () {
   expression = "";
   $("#math-result").val("");
 });
+
+
+function clearSignupInputs(){
+ 
+  $("#name-signup").val('');
+  $("#email-signup").val('');
+  $("#password-signup").val('');
+
+}
+
+
+$("#signup-button").click(async function(event){
+
+  event.preventDefault();
+
+  const form = document.querySelector(".needs-validation");
+  if (!form.checkValidity()) {
+    event.stopPropagation();
+    form.classList.add("was-validated");
+    return;
+  }
+
+  const data = {
+    name: $("#name-signup").val(),
+    email: $("#email-signup").val(),
+    password: $("#password-signup").val(),
+  };
+
+  try {
+
+   const response = await fetch(api_url_signup, {
+      method: "POST", 
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if(!response.ok) throw new Error();
+
+    console.log(response);
+
+    clearSignupInputs();
+
+    window.location.href = "sucess-signup.html";
+  } catch (err) {
+    $("#response-signup-button").html('<div class="alert alert-danger" role="alert">Erro ao tentar realizar o cadastro! Por favor confira as informações e tente novamente</div>');
+  }
+
+})
