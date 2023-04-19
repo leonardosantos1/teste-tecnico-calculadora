516f7956c1b99698e6228444e15c9b9607020136
Leonardo Santos <leonardo.santos574@hotmail.com>
2023-04-19 19:47:52 -0300

    implement logout routes and method

--- teste-tecnico-backend-l5network/src/routes/index.ts
+++ teste-tecnico-backend-l5network/src/routes/index.ts
@@ -3,6 +3,7 @@ import { loginRouter } from "./login.routes";
 import { signupRouter } from "./signup.routes";
 import { usersRouter } from "./users.routes";
 import { mathsRouter } from "./maths.routes";
+import { logoutRouter } from "./logout.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/login", loginRouter);
 router.use("/signup", signupRouter);
 router.use("/users", usersRouter);
 router.use("/maths",mathsRouter);
+router.use("/logout",logoutRouter);
 
 export { router };
---
+++ teste-tecnico-backend-l5network/src/routes/logout.routes.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response } from "express";
+import { ApplicationError } from "../error/ApplicationError";
+
+const logoutRouter = Router();
+
+logoutRouter.get("/", (req: Request, res: Response) => {
+    try {
+
+        res.clearCookie('token', { httpOnly: true });
+        res.clearCookie('name');
+        res.clearCookie('user_id');
+        return res.status(200).send();
+
+    } catch (err) {
+        console.log(err);
+        throw new ApplicationError(err, 500);
+    }
+})
+
+export {logoutRouter}
